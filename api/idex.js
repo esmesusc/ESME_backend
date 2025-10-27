19df4d177847b2009610ea584f5d2397fabf75a2
esmesusc <esmesusc25@gmail.com>
2025-10-27 23:17:55 +0300

    Adjusting to deployment 005

---
+++ api/idex.js
@@ -0,0 +1,36 @@
+const express = require('express')
+require('dotenv').config()
+const cors = require('cors')
+
+const connectDB = require('./db')
+
+
+const applicantsPath = require('./routes/applicantsRoute')
+const eventsPath = require('./routes/eventsRoute')
+const autherizationPath = require('./middleware/accessAuth')
+
+
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(cors({
+    origin: ['http://localhost:3000', 'https://esme-seven.vercel.app'],
+    credentials: true,
+}));
+
+
+app.use(('/api/applicants'), applicantsPath)
+app.use(('/api/events'), eventsPath)
+app.use(('/api/autherize'), autherizationPath)
+
+
+// const port = process.env.PORT || 4000
+// app.listen(port, () => {
+//     console.log('Server is running...');
+// })
+
+export default app;
