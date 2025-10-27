734aa38500b5022f3073860e702f086c6aa28998
esmesusc <esmesusc25@gmail.com>
2025-10-27 22:26:09 +0300

    Adjusting to deployment 002

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
-    origin: [process.env.FRONTEND_URL, 'http://localhost:4000'],
+    origin: [process.env.FRONTEND_URL, 'http://localhost:3000'],
     credentials: true
 }))
 
