18fffacb43e46e8417e3eee2537f78bb6d92169b
esmesusc <esmesusc25@gmail.com>
2025-10-28 00:05:05 +0300

    Adjusting to deployment 012

--- index.js
+++ index.js
@@ -33,9 +33,9 @@ app.get('/', (req, res) => {
 })
 
 
-const port = process.env.PORT || 4000
-app.listen(port, () => {
-    console.log('Server is running...');
-})
+// const port = process.env.PORT || 4000
+// app.listen(port, () => {
+//     console.log('Server is running...');
+// })
 
 module.exports = app;
