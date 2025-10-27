9f7ad6cd9bb2d30e88ded6ec104fcb9699f5e174
esmesusc <esmesusc25@gmail.com>
2025-10-27 22:41:17 +0300

    Adjusting to deployment 003

--- server.js
+++ server.js
@@ -12,14 +12,27 @@ const autherizationPath = require('./middleware/accessAuth')
 
 connectDB()
 
+
+const allowedOrigins = [
+    process.env.FRONTEND_URL,
+    'http://localhost:3000',
+];
+
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
 app.use(cors({
-    origin: [process.env.FRONTEND_URL, 'http://localhost:3000'],
-    credentials: true
-}))
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+}));
 
 
 app.use(('/applicants'), applicantsPath)
